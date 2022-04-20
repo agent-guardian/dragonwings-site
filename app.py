26e7d6d8382cecd453101de440e7be1f9f5d5b32
firnen0 <37234915+firnen0@users.noreply.github.com>
2022-04-19 20:26:42 -0400

    added all regular pages

--- app.py
+++ app.py
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('home.jinja')
+
+@app.route("/style/")
+def style():
+    return render_template('style.jinja')
+
+@app.route("/minecraft/")
+def minecraft():
+    return render_template('minecraft.jinja')
+
