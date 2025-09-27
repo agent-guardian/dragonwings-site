ef5de8b6159c2482e1a8551c80d6dc919744c471
Agent Guardian <37234915+agent-guardian@users.noreply.github.com>
2025-09-26 22:55:44 -0400

    error pages done

--- app.py
+++ app.py
@@ -15,3 +15,19 @@ def style():
 def minecraft():
     return render_template('minecraft.jinja')
 
+@app.errorhandler(400)
+def bad_request(e):
+    return render_template('400.jinja'), 400
+
+@app.errorhandler(401)
+def unauthorized(e):
+    return render_template('401.jinja'), 401
+
+@app.errorhandler(403)
+def forbidden(e):
+    return render_template('403.jinja'), 403
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.jinja'), 404
+
